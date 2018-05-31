153989a3bbd1717e2a863b0dd9a43c5c0ec5801f
dj940212 <2902273280@qq.com>
2018-05-31 14:17:41 +0800

    修改文案

--- app.js
+++ app.js
@@ -5,28 +5,8 @@ const Router = require('koa-router');
 const staticPath = './'
 
 const app = new Koa();
-const router = new Router();
-
-// router.get('/users/:id', (ctx, next) => {
-//   	ctx.body = "1242343"
-// 	next()
-// });
-
-// app
-//   .use(router.routes())
-//   .use(router.allowedMethods());
-
 
 
 app.use(koaStatic(path.join( __dirname,  staticPath)))
-
-app.use((ctx, next) => {
-  return next().then(() => {
-    if (ctx.path === '/123') {
-      ctx.body = 'Try `GET /package.json`'
-    }
-  })
-})
-
 
 app.listen(8085)
